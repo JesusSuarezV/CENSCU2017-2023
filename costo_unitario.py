9d3668793fe6d331b0d2917cb3702387d417f721
JesusSuarezV <jesusalexandersv@ufps.edu.co>
2023-12-19 01:32:34 -0500

    Se añade el CU promedio por año

--- costo_unitario.py
+++ costo_unitario.py
@@ -7,12 +7,15 @@ def importarCSV(año):
     dataframe = pd.DataFrame(csv[["Mes", "Costo Unitario"]])
     mes = dataframe.head(12)
     print(mes)
+    promedio = round(mes["Costo Unitario"].mean(), 1)
+    print(f"CU Promedio: {promedio}")
 
     x = csv["Mes"]
     y = csv["Costo Unitario"]
 
-    plt.figure(figsize=(14, 7))
-    plt.xlabel('Mes', fontsize=18)
+    plt.figure(figsize=(14, 10))
+    plt.xlabel(
+        f'Mes \n\n El Costo Unitario Promedio fue de {promedio}', fontsize=18)
     plt.ylabel('Costo Unitario (En Pesos por kWh)', fontsize=16)
     plt.scatter(x, y)
     plt.plot(x, y)
